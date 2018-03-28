8b6b5f46c924705a40039db5ee38236d36a9bd25
Raymond Okwenda <raymondokwenda@Raymonds-MacBook-Pro.local>
2018-03-29 02:55:46 +0300

    add class User

---
+++ user.py
@@ -0,0 +1,18 @@
+class User:
+    '''
+    class that generates new instance of user
+    '''
+    user_list = [] # Empty user list
+
+    def  __init__(self,login_name, password):
+    # '''
+    # __init__ method that help us define properties for our objects
+    #
+    # Args:
+    #     login_name: New user login_name.
+    #     password: New user password.
+    # '''
+
+
+        self.login_name =login_name
+        self.password =password
